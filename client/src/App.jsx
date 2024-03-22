b61a2886a200ca74e439f479084548621da5e700
Somnath Roy <somnathroy0340@gmail.com>
2024-03-22 11:24:42 +0530

    Template Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,42 @@
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+// React Router
+import { Route, Routes, useNavigate } from "react-router-dom";
+// Components
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/Common/Navbar";
+
+//Video Call
+// import CallPage from "./components/VideoCallFeature/CallPage";
+// import RoomPage from "./components/VideoCallFeature/RoomPage";
 
 function App() {
 
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="flex min-h-screen w-screen flex-col bg-purple-300 font-inter">
+      <div className="bg-purple-800">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="login" element={<Login />} />
+
+        <Route path="signup" element={<Signup />} />
+
+        {/* 404 Page */}
+        {/* <Route path="*" element={<Error />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      Contact us Page
+    </div>
+  )
+}
+
+export default Contact
