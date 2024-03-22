d3a44fe2d5b46ecd069c1ade737dcff5e17d4e99
sahanur1111 <118846504+sahanur1111@users.noreply.github.com>
2024-03-22 17:33:41 +0530

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,25 @@ import Home from "./pages/Home";
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import Navbar from "./components/Common/Navbar";
+import Footer from "./components/Common/Footer";
 
 //Video Call
 // import CallPage from "./components/VideoCallFeature/CallPage";
 // import RoomPage from "./components/VideoCallFeature/RoomPage";
 
 function App() {
-
   return (
     <div>
-        <Navbar />
+      <Navbar />
+      <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
+      </div>
+      <hr />
+      <Footer />
     </div>
   );
 }
