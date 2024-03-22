0101f3ce91d6a27da3d4fad3f5c48188eab763d8
Somnath Roy <somnathroy0340@gmail.com>
2024-03-22 11:51:44 +0530

    Template Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-import { useEffect } from "react";
 import "./App.css";
-// Redux
-import { useDispatch, useSelector } from "react-redux";
-// React Router
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 // Components
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
 import Navbar from "./components/Common/Navbar";
 
 //Video Call
@@ -17,24 +13,13 @@ import Navbar from "./components/Common/Navbar";
 function App() {
 
   return (
-    <div className="flex min-h-screen w-screen flex-col bg-purple-300 font-inter">
-      <div className="bg-purple-800">
+    <div>
         <Navbar />
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/about" element={<About />} />
-
-        <Route path="/contact" element={<Contact />} />
-
-        <Route path="login" element={<Login />} />
-
-        <Route path="signup" element={<Signup />} />
-
-        {/* 404 Page */}
-        {/* <Route path="*" element={<Error />} /> */}
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
     </div>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home Page
+     <h1> Home Page</h1>
     </div>
   )
 }
