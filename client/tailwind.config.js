6e213f12a32ab21bf8009dbc5741fa3922c69e0b
sahanur1111 <118846504+sahanur1111@users.noreply.github.com>
2024-03-22 15:40:31 +0530

    Navbar added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue":"#90E0EF",
+        "red":"#FF0000",
+        "secondary":"#555",
+        "primaryBG":"#FCFCFC"  
+      },
+      fontFamily: {
+           "primary":['Inter', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
